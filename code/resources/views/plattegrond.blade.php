a9defe2e51ddaf0f64f5ac6c645440bf571cccc9
nathanneve <nathanneve@live.be>
2023-10-02 10:53:36 +0200

    view + path plattegrondpagina

---
+++ code/resources/views/plattegrond.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>plattegrond</title>
+</head>
+<body>
+    <header>
+        <!-- Your header content goes here -->
+    </header>
+
+    <nav>
+        <!-- Your navigation menu goes here -->
+    </nav>
+
+    <main>
+
+        <h1>plattegrond</h1>
+
+    </main>
+
+    <footer>
+        <!-- Your footer content goes here -->
+    </footer>
+
+    <!-- Add your JavaScript includes and scripts here -->
+    <!--<script src="{{ asset('js/app.js') }}"></script> -->
+
+</body>
+</html>
