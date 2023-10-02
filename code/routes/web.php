6b68a405de17b000dc8b6b8e40c3610504dd3a76
nathanneve <nathanneve@live.be>
2023-10-02 16:24:21 +0200

    fixed the linking of css & js

--- code/routes/web.php
+++ code/routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::get('/plattegrond', function () {
     return view('plattegrond');
 });
+
+Route::get('/werkplek', function () {
+    return view('werkplek');
+});
+
