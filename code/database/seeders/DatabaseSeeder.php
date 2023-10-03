ff8494eb8262a75607f89617bc0ca0edc42f811e
nathanneve <nathanneve@live.be>
2023-10-03 10:00:14 +0200

    seeders

--- code/database/seeders/DatabaseSeeder.php
+++ code/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
       $this->call([
          RoleSeeder::class,
          UserSeeder::class,
+         werkplekSeeder::class,
       ]);
    }
 }
---
+++ code/database/seeders/DierSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DierSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('dier')->insert([
+            'werkplekid' => 1,
+            'diersoortid' => 1,
+            'quarantaine' => 0,
+            'inventarisid' => 1,
+        ]);
+    }
+}
