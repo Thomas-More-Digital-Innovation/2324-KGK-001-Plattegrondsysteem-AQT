14b29fd0028eb8996367fd2ddf73e64700d50ea5
Roel Metdepenningen <r0893183@student.thomasmore.be>
2023-10-04 12:31:59 +0200

    aanpassing logo en vertaling

--- code/resources/views/auth/forgot-password.blade.php
+++ code/resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Wachtwoord vergeten? Geen probleem. Geef uw email adres in en wij sturen u een link om een nieuw wachtwoord te kiezen.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Stuur reset link') }}
             </x-primary-button>
         </div>
     </form>
--- code/resources/views/auth/verify-email.blade.php
+++ code/resources/views/auth/verify-email.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __('Bedankt om te registreren! Gelieve voor dat we beginnen je email adres te verifiëren door op de link te klikken die we zojuist naar u hebben gemailed. Als u geen mail hebt ontvangen kunnen we een nieuwe sturen.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            {{ __('Er is een verificatielink gestuurd naar het gegeven email adres.') }}
         </div>
     @endif
 
@@ -15,7 +15,7 @@
 
             <div>
                 <x-primary-button>
-                    {{ __('Resend Verification Email') }}
+                    {{ __('Verificatie mail opnieuw sturen') }}
                 </x-primary-button>
             </div>
         </form>
@@ -24,7 +24,7 @@
             @csrf
 
             <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
+                {{ __('Afmelden') }}
             </button>
         </form>
     </div>
