02baaaf86bf93f563a1c5364abfdd1814231c540
Hzia <tom.bulen@gmail.com>
2023-10-07 17:13:33 +0200

    update composer & fix protocol colors

--- code/resources/views/components/protocollen/protocoltypes.blade.php
+++ code/resources/views/components/protocollen/protocoltypes.blade.php
@@ -3,8 +3,8 @@
       $types = json_decode(DB::table('protocoltype')->get());
       foreach ($types as $v) {
          ?>
-         <a href="/protocoltype?id={{$v->id}}&t={{$v->name}}&c={{$v->color}}" class="my-5 mx-32 flex justify-center items-center flex-col">
-            <div class="ring-2 ring-black w-72 bg-[#{{$v->color}}] h-72 flex justify-center items-center rounded-3xl">
+         <a href="/protocoltype?id={{$v->id}}&t={{$v->name}}&c={{$v->color}}" class="hover:scale-105 transition duration-150 ease-out hover:ease-in my-5 mx-32 flex justify-center items-center flex-col">
+            <div class="ring-2 ring-black w-72 h-72 flex justify-center items-center rounded-3xl" style="background-color:#{{$v->color}};">
                <iconify-icon icon="{{$v->icon}}" height="200"></iconify-icon>
             </div>
             <h3 class="text-3xl text-center font-bold">{{$v->name}}</h3>
--- code/resources/views/components/titlebar.blade.php
+++ code/resources/views/components/titlebar.blade.php
@@ -1,3 +1,3 @@
-<h1 class="fixed left-0 right-0 text-3xl h-14 bg-[#{{$color}}] border-b-2 border-black flex justify-center items-center p-2 ms-40">
+<h1 class="fixed left-0 right-0 text-3xl h-14 border-b-2 border-black flex justify-center items-center p-2 ms-40" style="background-color:#{{$color}};">
    {{$title}}
 </h1>
