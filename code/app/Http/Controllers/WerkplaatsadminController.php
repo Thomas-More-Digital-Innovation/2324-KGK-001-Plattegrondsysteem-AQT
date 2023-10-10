13d83f7dd2a057f06fdc764757d786e99cb1f0ee
nathanneve <nathanneve@live.be>
2023-10-10 13:19:20 +0200

    werkplekadmin af

---
+++ code/app/Http/Controllers/WerkplaatsadminController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class WerkplaatsadminController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $roleID = Auth::user()->roleid;
+            if ($roleID == 4) {
+                $werkplek = DB::table('werkplek')->get();
+                return view('werkplaatsadmin', ['werkplek' => $werkplek]);
+            } else {
+                abort(401);
+            }
+        }
+    }
+
+    public function updateWorkplaceStatus(Request $request)
+    {
+        if (Auth::id()) {
+            $roleID = Auth::user()->roleid;
+            if ($roleID == 4) {
+                $activeWorkplaces = $request->input('active', []);
+
+                DB::table('werkplek')
+                    ->whereIn('id', $activeWorkplaces)
+                    ->update(['active' => true]);
+
+                DB::table('werkplek')
+                    ->whereNotIn('id', $activeWorkplaces)
+                    ->update(['active' => false]);
+
+                return redirect()->route('werkplaatsadmin.index')->with('success', 'Workplace status updated successfully');
+            } else {
+                abort(401);
+            }
+        }
+    }
+}
+
--- code/routes/web.php
+++ code/routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\voedselsoortenController;
 use App\Http\Controllers\voederrichtlijnenController;
-
+use App\Http\Controllers\WerkplaatsadminController;
 use App\Http\Controllers\DiersoortController;
 
 /*
@@ -111,6 +111,12 @@ Route::get('admin', [HomeController::class, 'adminhome'])->middleware('auth')->n
 // view pages
 Route::get('/admin/protocollen', [HomeController::class, 'protocoladmin'])->middleware('auth')->name('protocoladmin');
 Route::get('/admin/protocollen/edit/{id}', [HomeController::class, 'protocoledit'])->middleware('auth')->name('protocoledit');
+
+// admin werkplek
+Route::get('werkplaatsadmin', [WerkplaatsadminController::class, 'index'])->name('werkplaatsadmin');
+Route::get('werkplaatsadmin', [WerkplaatsadminController::class, 'index'])->name('werkplaatsadmin.index');
+Route::post('werkplaatsadmin/update', [WerkplaatsadminController::class, 'updateWorkplaceStatus'])->name('werkplaatsadmin.update');
+
 
 // data handlers
 Route::post('/admin/protocollen/add/', [HomeController::class, 'protocoladd'])->middleware('auth')->name('protocoladd');
