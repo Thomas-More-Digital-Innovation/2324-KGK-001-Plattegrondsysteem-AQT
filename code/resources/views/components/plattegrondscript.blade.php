16dbfe7719f119b14d289f42e2548ade7fc83278
Roel Metdepenningen <r0893183@student.thomasmore.be>
2023-10-11 17:03:21 +0200

    created js check for inactive werkplek

---
+++ code/resources/views/components/plattegrondscript.blade.php
@@ -0,0 +1,29 @@
+<?php
+    $werkplek = DB::table('werkplek')->get();
+    $werkplekData = $werkplek->map(function ($item) {
+        return [
+            'name' => $item->name,
+            'active' => $item->active,
+        ];
+    });
+?>
+<script>
+    const werkplekData = @json($werkplekData);
+    const wps = document.querySelectorAll('[id^="wp"], [id^="bio"]');
+    wps.forEach(wp => {
+        const id = wp.getAttribute('id');
+        const wpData = werkplekData.find(wpData => wpData.name === id.replace(/[xy]/g, ''));
+        if (wpData) {
+            if (wpData.active == 1) {
+                const defaultColor = wp.getAttribute('fill');
+                wp.addEventListener("mouseenter", function() { wp.setAttribute('fill', '#ddd') });
+                wp.addEventListener("mouseleave", function() { wp.setAttribute('fill', defaultColor) });
+                wp.addEventListener("click", function() {
+                    window.location.href = "werkplek?id=" + id;
+                });
+            } else {
+                wp.setAttribute('fill', '#949494');
+            };
+        };
+    });
+</script>
