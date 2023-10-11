6d4383d99b8a5b0b8f906ad4a1b9694ceddb2702
MattBoeren <r0932402@student.thomasmore.be>
2023-10-11 14:03:01 +0200

    update password done

--- code/resources/views/profile/partials/update-password-form.blade.php
+++ code/resources/views/profile/partials/update-password-form.blade.php
@@ -2,15 +2,26 @@
 
     <div class="grid place-items-center">
         <header class="mb-10">
-            <h2>
+            <h2 class="text-center">
                 Wachtwoord veranderen
             </h2>
 
-            <p>
+            <p class="text-center">
                 Gebruik minimaal 8 karakters
             </p>
         </header>
 
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        @if(session('error'))
+            <div class="alert alert-success">
+                {{ session('error') }}
+            </div>
+        @endif
         <form method="post" action="{{ route('password.update') }}" class=" md:flex flex-col md:justify-center w-80">
             @csrf
             @method('put')
@@ -19,9 +30,9 @@
             <input id="current_password" name="current_password" type="password" autocomplete="current-password" class="text-center"/>
 
             <label for="password" class="text-center">Nieuw wachtwoord:</label>
-            <input id="password" name="password" type="password"  autocomplete="new-password" class="text-center"/>
+            <input id="password" minlength="8" name="password" type="password"  autocomplete="new-password" class="text-center"/>
 
-            <label for="password_confirmation" class="text-center">Bevestig nieuw wachtwoord:</label>
+            <label for="password_confirmation" minlength="8" class="text-center">Bevestig nieuw wachtwoord:</label>
             <input id="password_confirmation" name="password_confirmation" type="password"  autocomplete="new-password" class="text-center"/>
 
             <input type="submit" value="Bevestigen">
