265b671cdf9734a4524c8d314375ba545b07bbb5
MattBoeren <r0932402@student.thomasmore.be>
2023-10-16 15:57:54 +0200

    done

--- code/resources/views/components/errorhandler.blade.php
+++ code/resources/views/components/errorhandler.blade.php
@@ -7,6 +7,17 @@
         </div>
     @endif
 </div>
+
+<div class="fixed top-16 left-44 z-50" id="succes-container">
+    @if(session('succes'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Succes!</strong>
+            <span class="block sm:inline">{{ session('succes') }}</span>
+            <button class="ml-auto px-2 py-1 rounded-md bg-green-500 text-white hover:bg-green-600 focus:outline-none" onclick="hideSuccesMessage()"><b>Sluiten</b></button>
+        </div>
+    @endif
+</div>
+
 <script>
     function hideErrorMessage() {
         var errorContainer = document.getElementById('error-container');
@@ -14,4 +25,11 @@
             errorContainer.style.display = 'none';
         }
     }
+    function hideSuccesMessage() {
+        var succesContainer = document.getElementById('succes-container');
+        if (succesContainer) {
+            succesContainer.style.display = 'none';
+        }
+    }
+    
 </script>
