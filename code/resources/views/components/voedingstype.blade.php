6193b9ce708b8629a4b210782b240246e8698a99
Tom Bulen <tom.bulen@gmail.com>
2023-10-20 09:23:35 +0200

    styling fix

--- code/resources/views/components/voedingstype.blade.php
+++ code/resources/views/components/voedingstype.blade.php
@@ -10,11 +10,11 @@ $voedingstype = DB::table('voedingstype')
 ?>
 
 <x-titlebar title='{{$voedingsname->name}}' color='{{$voedingsname->color}}' back=true link='/voederrichtlijnen'/>
-<div class="flex items-center justify-center h-screen pt-14">
-    <div class="flex flex-wrap justify-center">
+<div class="flex items-center justify-center pt-14">
+    <div class="flex flex-wrap justify-center h-48">
     @foreach ($voedingstype as $voeding)
         <div class="m-5">
-            <img src={{$voeding->icon}} alt="Icon">
+            <img src={{$voeding->icon}} alt="Icon" class="h-40">
             <p class="text-center p-2" style="background-color:#{{$voedingsname->color}};">{{$voeding->name}}</p>
         </div>
     @endforeach
--- code/resources/views/components/voedselsoorten.blade.php
+++ code/resources/views/components/voedselsoorten.blade.php
@@ -3,7 +3,7 @@
         $voedingstype = DB::table('voedingsrichtlijnen')->get();
         foreach ($voedingstype as $type) {
             ?>
-                <a href="/voedsel?id={{$type->id}}" class="hover:scale-105 transition duration-150 ease-out hover:ease-in my-5 mx-32 flex justify-center items-center flex-col">
+                <a href="/voedsel?id={{$type->id}}" class="w-72 hover:scale-105 transition duration-150 ease-out hover:ease-in my-5 mx-32 flex justify-center items-center flex-col">
                     <div class="ring-2 ring-black w-72 h-72 flex justify-center items-center rounded-3xl" style="background-color:#{{$type->color}};">
                         <iconify-icon icon="{{$type->icon}}" height="200" width="200"></iconify-icon>
                     </div>
