8762b98a86d16f36ebde24cd1c71c6f9c335427a
Hzia <tom.bulen@gmail.com>
2023-12-05 17:47:59 +0100

    fix charts timezone

--- code/resources/js/biomedisch.js
+++ code/resources/js/biomedisch.js
@@ -1,10 +1,10 @@
 const gewichtInput = document.getElementById('biomedisch-gewicht');
 const temperatuurInput = document.getElementById('biomedisch-temperatuur');
-
+let DateTime = luxon.DateTime;
 //doorsturen van het gewicht van een dier
 gewichtInput.addEventListener("change", function() {
-    const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ");
+    const local = DateTime.now().setZone('Europe/Brussels');
+    const formattedDate = local.toFormat('yyyy-MM-dd HH:mm:ss');
     const changedElement = event.target;
     const newValue = changedElement.value;
     const inputType = gewichtInput.getAttribute('name');
@@ -14,8 +14,8 @@ gewichtInput.addEventListener("change", function() {
 
 //doorsturen van de temperatuur van een dier
 temperatuurInput.addEventListener("change", function() {
-    const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ");
+    const local = DateTime.now().setZone('Europe/Brussels');
+    const formattedDate = local.toFormat('yyyy-MM-dd HH:mm:ss');
     const changedElement = event.target;
     const newValue = changedElement.value;
     const inputType = temperatuurInput.getAttribute('name');
