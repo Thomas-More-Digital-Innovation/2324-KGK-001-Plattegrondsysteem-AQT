1ba68743049dedbe11e0ff9a9cd7d82b64207c1a
MattBoeren <r0932402@student.thomasmore.be>
2023-12-12 12:10:39 +0100

    Update opvolginghome.blade.php

--- code/resources/views/components/opvolging/opvolginghome.blade.php
+++ code/resources/views/components/opvolging/opvolginghome.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('title', 'Admin: Opvolging')
 @section('content')
-   <x-titlebar title="Admin: Opvolging" color="FF7E7E" back=true link="./admin"/>
+   <x-titlebar title="Admin: Opvolging" color="FF7E7E" back=true link="../admin"/>
    @include('components.opvolging.infotabel')
 @endsection
