e4d2a838bdf9622077664677983660c6538b964b
Wesley Van Laer <r0929447@student.thomasmore.be>
2023-12-18 11:15:01 +0100

    favicon op login+ plattegrond lijn

--- code/resources/views/auth/login.blade.php
+++ code/resources/views/auth/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/public/favicon.ico">
     <title>Login</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
