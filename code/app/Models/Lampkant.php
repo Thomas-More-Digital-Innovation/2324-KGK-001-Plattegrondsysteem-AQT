1988939b579cde996728346f916c8a4288404266
Nathan Neve <113888268+NathanNeve@users.noreply.github.com>
2024-02-14 09:15:46 +0100

    Update Lampkant.php

--- code/app/Models/Lampkant.php
+++ code/app/Models/Lampkant.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Lampkant extends Model
 {
     // deze model is voor de koppeling van lampen aan inventaris
-    // associatietabellen hebben typisch geen model nodig, maar in dit geval is het handig om de koppelingen te kunnen opvragen in onze niet zo fantastische programmeerlogica
+    // associatietabellen hebben typisch geen model nodig, 
+    // maar in dit geval is het handig om de koppelingen te kunnen 
+    // opvragen in onze niet zo fantastische programmeerlogica
     
     use HasFactory;
 
